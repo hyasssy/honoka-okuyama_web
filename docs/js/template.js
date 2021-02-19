d3187ce3476d53d9c029803908b4bc1cc91a5560
Yuto Hayashi <hyasssy@gmail.com>
2021-02-20 02:05:32 +0900

    ベースコピペ

---
+++ docs/js/template.js
@@ -0,0 +1,10 @@
+// コピーライトのテンプレート
+function copyright() {
+    var html = "";
+    html += '<div class="copyright">';
+    html += '<span>©️ 2020, Yuto Hayashi</span>';
+    //html += '<a href="https://www.instagram.com/hyasssy" target="_blank" >Instagram </a>';
+    //html += '<a href="https://twitter.com/hyasssy" target="_blank" >Twitter</a>';
+    html += '</div>';
+    document.write(html);
+}
